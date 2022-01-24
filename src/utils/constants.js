8b504320f305741a465554025df899be67018381
이도현 <ksmfou98@naver.com>
2022-01-24 23:00:38 +0900

    feat: option 상수, API End Point 설정
    Co-authored-by: kykim00 <kykim00@users.noreply.github.com>

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export const FIRST_CALCULATOR_OPTIONS = [
+  {
+    key: "KRW",
+    value: "KRW",
+    name: "한국(KRW)",
+  },
+  { key: "JPY", value: "JPY", name: "일본(JPY)" },
+  {
+    key: "PHP",
+    value: "PHP",
+    name: "필리핀(PHP)",
+  },
+];
+
+export const EXCHANGE_RATE_API_URL =
+  "http://api.currencylayer.com/live?access_key=a47d3fe24319dccb1d87ff3bb23ec31a";
