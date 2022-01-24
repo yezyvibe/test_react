14e2df5cc50bb7ad2763dd1f5e0f826ce1458d99
이도현 <ksmfou98@naver.com>
2022-01-24 20:15:37 +0900

    feat: GlobalStyles 설정

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,6 +3,20 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
+    html,
+    body,
+    #root {
+    height: 100%;
+    }
+
+    * {
+    box-sizing: border-box;
+    }
+
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
 `;
 
 export default GlobalStyles;
